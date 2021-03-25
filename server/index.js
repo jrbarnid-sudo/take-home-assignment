25119785d18b0e3253e68e23d8f05d66e2fd1bdc
Jeff Barnhart <jrbarnid@gmail.com>
2021-03-25 09:25:02 -0400

    Adding initial server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Todo {
+    title: String
+    status: String
+    lastUpdatedAt: String
+    createdAt: String
+  }
+
+  type Query {
+    todos: [Todo]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    get: () => todos,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Listening on ${url}`);
+});
